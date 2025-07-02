f64bf5152552baa9758b2a8b6ff996a007f495c8
callng <ver@vip.qq.com>
2025-07-02 16:25:11 +0800

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.owo233.tcqt"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.owo233.tcqt"
+        minSdk = 27
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+        buildConfigField("String", "APP_NAME", "\"TCQT\"")
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    packaging {
+        resources.excludes.addAll(
+            arrayOf(
+                "kotlin/**",
+                "META-INF/**",
+                "**.bin",
+                "kotlin-tooling-metadata.json"
+            )
+        )
+    }
+
+    applicationVariants.all {
+        outputs.all {
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            output.outputFileName?.let { fileName ->
+                if (fileName.endsWith(".apk")) {
+                    val projectName = rootProject.name
+                    val versionName = defaultConfig.versionName
+                    output.outputFileName = "${projectName}_v${versionName}.apk"
+                }
+            }
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+            freeCompilerArgs.set(listOf(
+                "-Xno-call-assertions",
+                "-Xno-param-assertions",
+                "-Xno-receiver-assertions"
+            ))
+        }
+    }
+}
+
+dependencies {
+    compileOnly(libs.xposed.api)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.kotlinx.io.jvm)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.serialization.protobuf)
+    implementation(libs.protobuf.java)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://api.xposed.info")
+        maven("https://jitpack.io")
+        maven("https://repo1.maven.org/maven2")
+    }
+}
+
+rootProject.name = "TCQT"
+include(":app")
