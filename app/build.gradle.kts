9086ddf6aa23a98c277717d42837afcfe80a534f
callng <ver@vip.qq.com>
2025-07-05 08:18:02 +0800

    bump version 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.owo233.tcqt"
         minSdk = 27
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         buildConfigField("String", "APP_NAME", "\"TCQT\"")
     }
 
