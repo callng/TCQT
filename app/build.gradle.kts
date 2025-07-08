21c4b2af8fb02fea01320e80254bdd887b9a57bf
owo233 <ver@vip.qq.com>
2025-07-09 03:59:48 +0800

    bump version 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.owo233.tcqt"
         minSdk = 27
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
         buildConfigField("String", "APP_NAME", "\"TCQT\"")
     }
 
