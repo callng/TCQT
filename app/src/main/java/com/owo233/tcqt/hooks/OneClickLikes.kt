06181f9975ab1334d470a13afaec46091547b6a8
owo233 <ver@vip.qq.com>
2025-07-13 15:10:05 +0800

    fix(TIM): 移除在TIM上的一键点赞Hook
    TIM TM 不支持点赞这个行为

--- app/src/main/java/com/owo233/tcqt/hooks/OneClickLikes.kt
+++ app/src/main/java/com/owo233/tcqt/hooks/OneClickLikes.kt
@@ -7,6 +7,7 @@ import com.owo233.tcqt.ext.ActionProcess
 import com.owo233.tcqt.ext.IAction
 import com.owo233.tcqt.ext.hookMethod
 import com.owo233.tcqt.internals.QQInterfaces
+import com.owo233.tcqt.utils.PlatformTools
 import com.owo233.tcqt.utils.logE
 import com.tencent.mobileqq.activity.VisitorsActivity
 import com.tencent.mobileqq.data.CardProfile
@@ -18,39 +19,42 @@ import de.robv.android.xposed.XposedBridge
 class OneClickLikes: IAction {
 
     override fun onRun(ctx: Context) {
-        val vote = VoteHelper::class.java.declaredMethods.firstOrNull {
-            it.parameterCount == 2 && it.parameterTypes[0] == CardProfile::class.java && it.parameterTypes[1] == ImageView::class.java
-        }
-        val voteHelperField = VisitorsActivity::class.java.declaredFields.firstOrNull {
-            it.type == VoteHelper::class.java
-        }
-
-        if (vote == null || voteHelperField == null) {
-            logE(msg = "获取点赞方法失败")
-            return
-        }
-
-        voteHelperField.isAccessible = true
-        VisitorsActivity::class.java.hookMethod("onClick").before {
-            val view = it.args[0] as View
-            val tag = view.tag
-
-            if (tag == null || tag !is CardProfile) return@before
-
-            val voteHelper = voteHelperField.get(it.thisObject) as VoteHelper
-            for (i in 0..<getMaxCount()) {
-                vote.invoke(voteHelper, tag, view)
+        // TIM不支持点赞行为
+        if (PlatformTools.isMqq()) {
+            val vote = VoteHelper::class.java.declaredMethods.firstOrNull {
+                it.parameterCount == 2 && it.parameterTypes[0] == CardProfile::class.java && it.parameterTypes[1] == ImageView::class.java
+            }
+            val voteHelperField = VisitorsActivity::class.java.declaredFields.firstOrNull {
+                it.type == VoteHelper::class.java
             }
 
-            it.result = Unit
-        }
-
-        AbsProfileHeaderComponent::class.java.hookMethod("handleVoteBtnClickForGuestProfile").before {
-            for (i in 0..<getMaxCount()) {
-                XposedBridge.invokeOriginalMethod(it.method, it.thisObject, it.args)
+            if (vote == null || voteHelperField == null) {
+                logE(msg = "获取点赞方法失败")
+                return
             }
 
-            it.result = Unit
+            voteHelperField.isAccessible = true
+            VisitorsActivity::class.java.hookMethod("onClick").before {
+                val view = it.args[0] as View
+                val tag = view.tag
+
+                if (tag == null || tag !is CardProfile) return@before
+
+                val voteHelper = voteHelperField.get(it.thisObject) as VoteHelper
+                for (i in 0..<getMaxCount()) {
+                    vote.invoke(voteHelper, tag, view)
+                }
+
+                it.result = Unit
+            }
+
+            AbsProfileHeaderComponent::class.java.hookMethod("handleVoteBtnClickForGuestProfile").before {
+                for (i in 0..<getMaxCount()) {
+                    XposedBridge.invokeOriginalMethod(it.method, it.thisObject, it.args)
+                }
+
+                it.result = Unit
+            }
         }
     }
 
