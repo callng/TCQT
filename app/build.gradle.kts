6fee4c2b1be2c9ed5ffea1bdd00f19ce2724c8bc
owo233 <ver@vip.qq.com>
2025-09-10 08:22:16 +0800

    bump version 3.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
         minSdk = 27
         targetSdk = 36
         versionCode = providers.provider { getBuildVersionCode(rootProject) }.get()
-        versionName = "3.3"
+        versionName = "3.3.1"
         buildConfigField("String", "APP_NAME", "\"TCQT\"")
         buildConfigField("Long", "BUILD_TIMESTAMP", "${System.currentTimeMillis()}L")
     }
