4c866b4f8a63b9e66697b02b029c6233cf61afa7
owo233 <ver@vip.qq.com>
2025-11-04 20:13:18 +0800

    feat: 将 禁用内置浏览器网页拦截 视为模块功能
    现在不需要强制启用禁用内置浏览器网页拦截功能了，它将作为一个可选功能加入功能列表

--- app/src/main/java/com/owo233/tcqt/hooks/BrowserRestrictMitigation.kt
+++ app/src/main/java/com/owo233/tcqt/hooks/BrowserRestrictMitigation.kt
@@ -3,21 +3,32 @@ package com.owo233.tcqt.hooks
 import android.content.Context
 import android.os.Bundle
 import com.owo233.tcqt.annotations.RegisterAction
+import com.owo233.tcqt.annotations.RegisterSetting
+import com.owo233.tcqt.annotations.SettingType
 import com.owo233.tcqt.ext.ActionProcess
-import com.owo233.tcqt.ext.AlwaysRunAction
+import com.owo233.tcqt.ext.IAction
+import com.owo233.tcqt.generated.GeneratedSettingList
 import com.owo233.tcqt.utils.FuzzyClassKit
 import com.owo233.tcqt.utils.hookBeforeMethod
+import com.owo233.tcqt.utils.paramCount
 
 @RegisterAction
-class BrowserRestrictMitigation : AlwaysRunAction() {
+@RegisterSetting(
+    key = "browser_restrict_mitigation",
+    name = "禁用内置浏览器网页拦截",
+    type = SettingType.BOOLEAN,
+    desc = "允许在内置浏览器中访问非官方认可的网页。",
+    uiOrder = 26
+)
+class BrowserRestrictMitigation : IAction {
 
     override fun onRun(ctx: Context, process: ActionProcess) {
-        FuzzyClassKit.findMethodByClassPrefix(
+        (FuzzyClassKit.findMethodByClassPrefix(
             prefix = "com.tencent.mobileqq.webview.WebSecurityPluginV2",
             isSubClass = true
         ) { _, method ->
-            method.parameterCount == 1 && method.parameterTypes[0] == Bundle::class.java
-        }?.hookBeforeMethod {
+            method.paramCount == 1 && method.parameterTypes[0] == Bundle::class.java
+        } ?: error("BrowserRestrictMitigation: 找不到目标方法..")).hookBeforeMethod {
             val bundle = it.args[0] as Bundle
             if (bundle.getInt("jumpResult", 0) != 0) {
                 bundle.putInt("jumpResult", 0)
@@ -25,6 +36,8 @@ class BrowserRestrictMitigation : AlwaysRunAction() {
             }
         }
     }
+
+    override val key: String get() = GeneratedSettingList.BROWSER_RESTRICT_MITIGATION
 
     override val processes: Set<ActionProcess> get() = setOf(ActionProcess.TOOL)
 }
