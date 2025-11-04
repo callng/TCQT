b02780ee6917e035b9175946b5452de48dec4e6d
owo233 <ver@vip.qq.com>
2025-11-05 02:44:46 +0800

    fix(ReplyNoAt): 误拦截长按头像的@功能

--- app/src/main/java/com/owo233/tcqt/hooks/ReplyNoAt.kt
+++ app/src/main/java/com/owo233/tcqt/hooks/ReplyNoAt.kt
@@ -25,16 +25,40 @@ import com.tencent.mvi.base.route.MsgIntent
 class ReplyNoAt : IAction {
 
     override fun onRun(ctx: Context, process: ActionProcess) {
-        XpClassLoader.load("com.tencent.mvi.base.route.VMMessenger")!!
-            .declaredMethods.firstOrNull { method ->
-                method.returnType == Void.TYPE &&
-                        method.isPublic && method.paramCount == 1 &&
-                        method.parameterTypes[0] == MsgIntent::class.java
-            }!!.hookBeforeMethod {param ->
-                if (param.args[0] is InputAtMsgIntent.InsertAtMemberSpan) {
-                    param.result = Unit
+        val clazz = XpClassLoader.load("com.tencent.mvi.base.route.VMMessenger")
+            ?: error("ReplyNoAt: 找不到 VMMessenger 类.")
+
+        val target = clazz.declaredMethods.firstOrNull {
+            it.returnType == Void.TYPE &&
+                    it.isPublic &&
+                    it.paramCount == 1 &&
+                    it.parameterTypes[0] == MsgIntent::class.java
+        } ?: error("ReplyNoAt: 找不到指定方法.")
+
+        target.hookBeforeMethod { param ->
+            val arg = param.args[0]
+            if (arg !is InputAtMsgIntent.InsertAtMemberSpan) return@hookBeforeMethod
+
+            /**
+             * 虽然通过遍历堆栈来判断是否为回复引用可以快速修复长按头像无法使用艾特的问题。
+             * 但是遍历堆栈是一个非常离谱的操作，坦率地说，堆栈检测的性能非常差且不稳定。
+             * 寻找更精确的 Hook 点是更好的选择，但就目前来说，可以作为临时解决方案。
+             */
+            val keyword = "aio.input.reply"
+            val trace = Throwable().stackTrace
+            var isReply = false
+            for (e in trace) {
+                val name = e.className
+                if (name.indexOf(keyword) != -1) {
+                    isReply =true
+                    break
                 }
             }
+
+            if (isReply) {
+                param.result = Unit
+            }
+        }
     }
 
     override val key: String get() = GeneratedSettingList.REPLY_NO_AT
