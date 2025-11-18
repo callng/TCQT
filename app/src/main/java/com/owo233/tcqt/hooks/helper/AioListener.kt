8861907e7962526248c1129bf26647a2eb5b7189
owo233 <ver@vip.qq.com>
2025-11-19 07:57:57 +0800

    fix: 消息防撤回导致离线消息丢失
    当InfoSyncPush中同时有离线消息和撤回消息动作时会导致离线消息无法展示在聊天列表中.
    具体表现为: 好友明明私聊了你,但是上线后消息列表却没有好友的消息,必须进入好友聊天页面才会重新拉取消息.

--- app/src/main/java/com/owo233/tcqt/hooks/helper/AioListener.kt
+++ app/src/main/java/com/owo233/tcqt/hooks/helper/AioListener.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 
+@OptIn(DelicateCoroutinesApi::class)
 object AioListener {
 
     fun handleMsgPush(buffer: ByteArray, param: MethodHookParam) {
@@ -26,21 +27,14 @@ object AioListener {
         val operationInfoByteArray = msg.messageBody.operationInfo.toByteArray()
 
         when(msgType) {
-            528 -> when (subType) {
-                138 -> onC2CRecallByMsgPush(operationInfoByteArray, msgPush, param)
-            }
-            732 -> when (subType) {
-                17 -> onGroupRecallByMsgPush(operationInfoByteArray, msgPush, param)
-            }
-            166 -> when (subType) { // 好友私聊消息
-                11 -> GeneratedSettingList.DISABLE_FLASH_PIC
-                    .takeIf { GeneratedSettingList.getBoolean(it) }
-                    .let { onC2CFlashPicByMsgPush(buffer, msgPush, param) }
+            528 -> if (subType == 138) onC2CRecallByMsgPush(operationInfoByteArray, msgPush, param)
+            732 -> if (subType == 17) onGroupRecallByMsgPush(operationInfoByteArray, msgPush, param)
+            166 -> if (subType == 11 && GeneratedSettingList.getBoolean(GeneratedSettingList.DISABLE_FLASH_PIC)) {
+                onC2CFlashPicByMsgPush(buffer, msgPush, param)
             }
         }
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
     private fun onC2CFlashPicByMsgPush(
         buffer: ByteArray,
         msgPush: MsgPushOuterClass.MsgPush,
@@ -48,11 +42,7 @@ object AioListener {
     ) {
         param.args[1] = buffer
 
-        val contentList = msgPush
-            .qqMessage // 1
-            .messageBody // 3
-            .richMsg // 1
-            .msgContentList // 2
+        val contentList = msgPush.qqMessage.messageBody.richMsg.msgContentList // 1.3.1.2
 
         if (contentList.size >= 3 && contentList[2].myCustomField.mtType == 3) {
             GlobalScope.launchWithCatch{ // 闪照消息，由于闪照被视为正常图片，所以添加一条提示
@@ -60,13 +50,9 @@ object AioListener {
 
                 val operatorUid = msgPush.qqMessage.messageHead.senderUid
                 if (operatorUid == QQInterfaces.currentUid) return@launchWithCatch
-
                 val msgSeq = msgPush.qqMessage.messageContentInfo.msgSeqId
 
-                val contact = ContactHelper.generateContact(
-                    chatType = MsgConstant.KCHATTYPEC2C,
-                    id = operatorUid
-                )
+                val contact = ContactHelper.generateContact(MsgConstant.KCHATTYPEC2C, operatorUid)
                 LocalGrayTips.addLocalGrayTip(contact, JsonGrayBusiId.AIO_AV_C2C_NOTICE, LocalGrayTips.Align.CENTER) {
                     text("对方发送了一条闪照")
                     msgRef("消息", msgSeq.toLong())
@@ -75,13 +61,12 @@ object AioListener {
         }
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
     private fun onGroupRecallByMsgPush(
-        operationInfoByteArray: ByteArray,
+        operationInfoByteArray: ByteArray, // 1.3.2
         msgPush: MsgPushOuterClass.MsgPush,
         param: MethodHookParam
     ) {
-        val firstPart = operationInfoByteArray.copyOfRange(0, 7)
+        val firstPart = operationInfoByteArray.copyOfRange(0, 7) // 1.3.2.5 and 1.3.2.0
         val secondPart = operationInfoByteArray.copyOfRange(7, operationInfoByteArray.size)
 
         val operationInfo = QQMessageOuterClass
@@ -151,7 +136,6 @@ object AioListener {
         }
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
     private fun onC2CRecallByMsgPush(
         operationInfoByteArray: ByteArray,
         msgPush: MsgPushOuterClass.MsgPush,
@@ -193,29 +177,63 @@ object AioListener {
             LocalGrayTips.addLocalGrayTip(contact, JsonGrayBusiId.AIO_AV_C2C_NOTICE, LocalGrayTips.Align.CENTER) {
                 text("对方想撤回一条")
                 msgRef("消息", recallMsgSeq.toLong())
-                text(",已拦截")
+                text(", 已拦截")
             }
         }
     }
 
     fun handleInfoSyncPush(buffer: ByteArray, param: MethodHookParam) {
         val infoSyncPush = InfoSyncPushOuterClass.InfoSyncPush.parseFrom(buffer)
-        infoSyncPush.syncMsgRecall.syncInfoBodyList.forEach { syncInfoBody ->
-            syncInfoBody.msgList.forEach { qqMessage ->
-                val msgType = qqMessage.messageContentInfo.msgType
-                val subType = qqMessage.messageContentInfo.subSeq
-                if ((msgType == 732 && subType == 17) || (msgType == 528 && subType == 138)) {
-                    val newInfoSyncPush = infoSyncPush.toBuilder().apply {
-                        syncMsgRecall = syncMsgRecall.toBuilder().apply {
-                            for (i in 0 until syncInfoBodyCount) {
-                                setSyncInfoBody(
-                                    i, getSyncInfoBody(i).toBuilder().clearMsg().build()
-                                )
-                            }
-                        }.build()
-                    }.build()
+        if (infoSyncPush.pushFlag != 2) return // 只有 flag=2 的推送才可能附带撤回内容
 
-                    param.args[1] = newInfoSyncPush.toByteArray()
+        val interceptedC2CMsgSeqList = mutableListOf<Pair<String, Long>>() // 用与给私聊类型添加撤回灰条提示
+
+        val newInfoSyncPush = infoSyncPush.toBuilder().apply {
+            syncMsgRecall = syncMsgRecall.toBuilder().apply {
+                for (i in 0 until syncInfoBodyCount) {
+                    val bodyBuilder = getSyncInfoBody(i).toBuilder()
+                    val filteredMsg = bodyBuilder.msgList.filter { qqMessage ->
+                        val msgType = qqMessage.messageContentInfo.msgType
+                        val subType = qqMessage.messageContentInfo.subSeq
+
+                        val isRecall = (msgType == 732 && subType == 17) || (msgType == 528 && subType == 138)
+                        if (isRecall && msgType == 528) {
+                            val opInfo = qqMessage.messageBody.operationInfo
+                            val c2cRecall = QQMessageOuterClass
+                                .QQMessage
+                                .MessageBody
+                                .C2CRecallOperationInfo
+                                .parseFrom(opInfo)
+                            val msgSeq = c2cRecall.info.msgSeq.toLong()
+                            val senderUid = qqMessage.messageHead.senderUid
+                            interceptedC2CMsgSeqList.add(senderUid to msgSeq)
+                        }
+                        !isRecall
+                    }
+                    bodyBuilder.clearMsg().addAllMsg(filteredMsg)
+                    setSyncInfoBody(i, bodyBuilder.build())
+                }
+            }.build()
+        }.build()
+
+        param.args[1] = newInfoSyncPush.toByteArray()
+
+        if (interceptedC2CMsgSeqList.isNotEmpty()) {
+            GlobalScope.launchWithCatch {
+                interceptedC2CMsgSeqList.forEach { (senderUid, msgSeq) ->
+                    val contact = ContactHelper.generateContact(
+                        chatType = MsgConstant.KCHATTYPEC2C,
+                        id = senderUid
+                    )
+                    LocalGrayTips.addLocalGrayTip(
+                        contact,
+                        JsonGrayBusiId.AIO_AV_C2C_NOTICE,
+                        LocalGrayTips.Align.CENTER
+                    ) {
+                        text("对方想撤回一条")
+                        msgRef("消息", msgSeq)
+                        text(", 已拦截")
+                    }
                 }
             }
         }
