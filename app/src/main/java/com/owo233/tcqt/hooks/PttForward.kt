33c58010f0d375cbc515f4c8dc1a3473042636bd
owo233 <ver@vip.qq.com>
2025-12-07 23:08:48 +0800

    docs: 修正语音转发功能描述

--- app/src/main/java/com/owo233/tcqt/hooks/PttForward.kt
+++ app/src/main/java/com/owo233/tcqt/hooks/PttForward.kt
@@ -40,7 +40,7 @@ import kotlin.random.Random
     key = "ptt_forward",
     name = "允许转发语音消息",
     type = SettingType.BOOLEAN,
-    desc = "长按语音消息显示转发按钮，可以将语音消息转发给其他好友或群，一次支持最多选择9个会话目标转发。",
+    desc = "长按语音消息显示转发按钮，可以将语音消息转发给其他好友或群。",
     uiOrder = 15
 )
 class PttForward : IAction, OnMenuBuilder {
