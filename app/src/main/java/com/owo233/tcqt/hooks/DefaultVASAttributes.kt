192e37135a7af922e54b0f7aa97dfb11be96907d
owo233 <ver@vip.qq.com>
2025-12-17 19:40:47 +0800

    feat: 净化聊天界面QQ秀头像

--- app/src/main/java/com/owo233/tcqt/hooks/DefaultVASAttributes.kt
+++ app/src/main/java/com/owo233/tcqt/hooks/DefaultVASAttributes.kt
@@ -7,8 +7,10 @@ import com.owo233.tcqt.annotations.SettingType
 import com.owo233.tcqt.ext.ActionProcess
 import com.owo233.tcqt.ext.IAction
 import com.owo233.tcqt.generated.GeneratedSettingList
+import com.owo233.tcqt.hooks.base.load
 import com.owo233.tcqt.internals.QQInterfaces
 import com.owo233.tcqt.utils.hookAfterMethod
+import com.owo233.tcqt.utils.hookBeforeMethod
 import com.tencent.mobileqq.aio.msg.AIOMsgItem
 import com.tencent.qqnt.kernel.nativeinterface.MsgRecord
 
@@ -17,7 +19,7 @@ import com.tencent.qqnt.kernel.nativeinterface.MsgRecord
     key = "default_vas_attrs",
     name = "净化聊天界面装扮",
     type = SettingType.BOOLEAN,
-    desc = "禁用他人消息的个性化气泡、字体与头像挂件。",
+    desc = "禁用他人消息的个性化气泡、字体、QQ秀头像与头像挂件。",
     uiTab = "界面"
 )
 @RegisterSetting(
@@ -25,7 +27,7 @@ import com.tencent.qqnt.kernel.nativeinterface.MsgRecord
     name = "可选禁用",
     type = SettingType.INT_MULTI,
     defaultValue = "0",
-    options = "关闭净化气泡|关闭净化字体|关闭净化挂件"
+    options = "关闭净化气泡|关闭净化字体|关闭净化头像挂件|关闭净化QQ秀头像"
 )
 class DefaultVASAttributes : IAction {
 
@@ -59,6 +61,22 @@ class DefaultVASAttributes : IAction {
                     }
                 }
             }
+        }
+
+        if (options and (1 shl 3) == 0) {
+            // 新版超级QQ秀 应该是在 9.2.35 版本或以上才有的
+            load("com.tencent.mobileqq.ai.avatar.api.impl.AIAvatarSwitchApiImpl")
+                ?.hookBeforeMethod(
+                    "isQQShowEnableForAIO",
+                    Long::class.javaPrimitiveType,
+                    Int::class.javaPrimitiveType,
+                    Long::class.javaPrimitiveType
+                ) { param ->
+                    val uin = (param.args[2] as Long).toString()
+                    if (uin != QQInterfaces.currentUin) {
+                        param.result = false
+                    }
+                }
         }
     }
 
