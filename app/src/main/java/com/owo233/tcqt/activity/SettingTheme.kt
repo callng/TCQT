cb9580f1fb2ac1671110e6e6fddb7924815f07a3
owo233 <ver@vip.qq.com>
2026-06-22 03:33:05 +0800

    chore: 处于夜间主题时禁用 dynamicColor

--- app/src/main/java/com/owo233/tcqt/activity/SettingTheme.kt
+++ app/src/main/java/com/owo233/tcqt/activity/SettingTheme.kt
@@ -3,7 +3,6 @@ package com.owo233.tcqt.activity
 import android.os.Build
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -18,8 +17,8 @@ fun SettingTheme(
 ) {
     val context = LocalContext.current
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        dynamicColor && !darkTheme && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            dynamicLightColorScheme(context)
         }
         darkTheme -> DarkSettingColors
         else -> LightSettingColors
