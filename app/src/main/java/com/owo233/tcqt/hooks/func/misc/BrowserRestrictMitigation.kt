93f1b42adf41752050b7c85c5a76c3d3683f3ae0
owo233 <ver@vip.qq.com>
2026-06-28 15:20:06 +0800

    fix: 禁用内置浏览器网页拦截

--- app/src/main/java/com/owo233/tcqt/hooks/func/misc/BrowserRestrictMitigation.kt
+++ app/src/main/java/com/owo233/tcqt/hooks/func/misc/BrowserRestrictMitigation.kt
@@ -1,49 +1,42 @@
 package com.owo233.tcqt.hooks.func.misc
 
 import android.app.Application
-import android.os.Bundle
 import com.owo233.tcqt.annotations.RegisterAction
 import com.owo233.tcqt.ext.ActionProcess
 import com.owo233.tcqt.ext.IAction
-import com.owo233.tcqt.utils.dexkit.DexKitTask
-import com.owo233.tcqt.utils.hook.hookBefore
-import org.luckypray.dexkit.query.FindMethod
-import org.luckypray.dexkit.query.base.BaseMatcher
-import org.luckypray.dexkit.query.enums.StringMatchType
-import java.lang.reflect.Modifier
+import com.owo233.tcqt.hooks.base.toClass
+import com.owo233.tcqt.utils.hook.hookReplace
+import com.owo233.tcqt.utils.hook.invokeOriginal
+import com.owo233.tcqt.utils.reflect.findMethod
 
 @RegisterAction
-class BrowserRestrictMitigation : IAction, DexKitTask {
+class BrowserRestrictMitigation : IAction {
 
+    private var targetUrl: String? = null
+
+    override val key: String get() = "browser_restrict_mitigation"
     override val name: String get() = "禁用内置浏览器网页拦截"
     override val desc: String get() = "允许在内置浏览器中访问非官方认可的网页。"
     override val uiTab: String get() = "杂项"
+    override val processes: Set<ActionProcess> get() = setOf(ActionProcess.TOOL)
 
     override fun onRun(app: Application, process: ActionProcess) {
-        requireMethod("browser_restrict_mitigation").hookBefore {
-            val bundle = it.args[0] as Bundle
-            if (bundle.getInt("jumpResult", 0) != 0) {
-                bundle.putInt("jumpResult", 0)
-                bundle.putString("jumpUrl", "")
+        "com.tencent.biz.pubaccount.CustomWebView".toClass.findMethod {
+            name = "loadUrl"
+            paramTypes = arrayOf(string)
+        }.hookReplace { param ->
+            val url = param.args[0] as String
+
+            if (url.contains(BLOCK_URL)) {
+                return@hookReplace param.invokeOriginal(arrayOf(targetUrl))
+            } else {
+                targetUrl = url
+                return@hookReplace param.invokeOriginal()
             }
         }
     }
 
-    override val key: String get() = "browser_restrict_mitigation"
-    override val processes: Set<ActionProcess> get() = setOf(ActionProcess.TOOL)
-
-    override fun getQueryMap(): Map<String, BaseMatcher> = mapOf(
-        "browser_restrict_mitigation" to FindMethod().apply {
-            searchPackages("com.tencent.mobileqq.webview")
-            matcher {
-                declaredClass(
-                    "com.tencent.mobileqq.webview.WebSecurityPluginV2",
-                    StringMatchType.StartsWith
-                )
-                name = "callback"
-                paramTypes = listOf("android.os.Bundle")
-                modifiers = Modifier.PUBLIC
-            }
-        }
-    )
+    private companion object {
+        const val BLOCK_URL = "c.pc.qq.com"
+    }
 }
