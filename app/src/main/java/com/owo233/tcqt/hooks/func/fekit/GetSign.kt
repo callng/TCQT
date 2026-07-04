723796d20a113079e791e7f77bb1494ba8bb44f3
owo233 <ver@vip.qq.com>
2026-07-05 00:32:06 +0800

    chore: make getsign happy

--- app/src/main/java/com/owo233/tcqt/hooks/func/fekit/GetSign.kt
+++ app/src/main/java/com/owo233/tcqt/hooks/func/fekit/GetSign.kt
@@ -6,8 +6,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import com.owo233.tcqt.HookEnv
@@ -19,23 +17,28 @@ import com.owo233.tcqt.ext.toHexString
 import com.owo233.tcqt.hooks.base.toClass
 import com.owo233.tcqt.internals.QQInterfaces
 import com.owo233.tcqt.loader.ReceiverRegistry
-import com.owo233.tcqt.utils.QQVersion
 import com.owo233.tcqt.utils.SyncUtils
 import com.owo233.tcqt.utils.dexkit.DexKitTask
 import com.owo233.tcqt.utils.hook.hookAfter
 import com.owo233.tcqt.utils.log.Log
 import com.owo233.tcqt.utils.reflect.findField
 import com.owo233.tcqt.utils.reflect.findMethod
-import com.tencent.mobileqq.msf.core.c0.a as QSign
-import com.tencent.mobileqq.msf.core.d0.a as TSign
-import com.tencent.mobileqq.msf.core.security.a as NSign
+import com.owo233.tcqt.utils.reflect.invokeAs
+import com.owo233.tcqt.utils.reflect.new
 import com.tencent.mobileqq.msf.service.MsfService
+import com.tencent.mobileqq.sign.QQSecuritySign
 import com.tencent.qphone.base.remote.ToServiceMsg
+import org.luckypray.dexkit.DexKitBridge
 import org.luckypray.dexkit.query.FindMethod
-import org.luckypray.dexkit.query.base.BaseMatcher
 
 @RegisterAction
 class GetSign : IAction, DexKitTask, InputRootInitCallback {
+
+    private var pendingEditText: EditText? = null
+    private val signer by lazy {
+        val method = requireMethod("getSign")
+        method.declaringClass.new() to method
+    }
 
     override val key: String get() = "get_sign"
     override val name: String get() = "获取测试签名"
@@ -51,8 +54,6 @@ class GetSign : IAction, DexKitTask, InputRootInitCallback {
         }
     }
 
-    private var pendingEditText: EditText? = null
-
     @SuppressLint("UnspecifiedRegisterReceiverFlag")
     private fun initMainProcess(app: Application) {
         val method = if (HookEnv.isTim()) {
@@ -63,26 +64,19 @@ class GetSign : IAction, DexKitTask, InputRootInitCallback {
 
         method.hookAfter { param ->
             val sendBtn = runCatching {
-                param.thisObject::class.java.findField { type = Button::class.java }.get(param.thisObject) as? Button
+                param.thisObject::class.java
+                    .findField { type = Button::class.java }
+                    .get(param.thisObject) as? Button
             }.getOrNull() ?: return@hookAfter
 
             val editText = runCatching {
-                param.thisObject::class.java.findField { type = EditText::class.java }.get(param.thisObject) as? EditText
+                param.thisObject::class.java
+                    .findField { type = EditText::class.java }
+                    .get(param.thisObject) as? EditText
             }.getOrNull() ?: return@hookAfter
 
-            val inputRoot = runCatching {
-                param.thisObject::class.java.declaredFields
-                    .filter { ViewGroup::class.java.isAssignableFrom(it.type) }
-                    .firstNotNullOfOrNull { field ->
-                        runCatching {
-                            field.isAccessible = true
-                            field.get(param.thisObject) as? ViewGroup
-                        }.getOrNull()
-                    }
-            }.getOrNull() ?: return@hookAfter
-
-            sendBtn.setOnLongClickListener { _ ->
-                onBtnLongClick(editText.text.toString(), sendBtn, editText, inputRoot)
+            sendBtn.setOnLongClickListener {
+                onBtnLongClick(sendBtn, editText)
                 return@setOnLongClickListener true
             }
         }
@@ -93,7 +87,7 @@ class GetSign : IAction, DexKitTask, InputRootInitCallback {
                 val sign = intent.getStringExtra("sign") ?: return
                 val error = intent.getStringExtra("error")
                 SyncUtils.runOnUiThread {
-                    val target = pendingEditText ?: getActiveEditText()
+                    val target = pendingEditText ?: getAIOEditText()
                     if (target != null) {
                         if (error != null) {
                             target.setText("签名获取失败: $error")
@@ -111,10 +105,8 @@ class GetSign : IAction, DexKitTask, InputRootInitCallback {
 
     @SuppressLint("SetTextI18n")
     override fun onBtnLongClick(
-        text: String,
         sendBtn: Button,
-        editText: EditText,
-        inputRoot: ViewGroup
+        editText: EditText
     ) {
         pendingEditText = editText
 
@@ -139,15 +131,12 @@ class GetSign : IAction, DexKitTask, InputRootInitCallback {
                         putWupBuffer(buffer)
                         requestSsoSeq = seq
                     }
-                    val sign: String = if (HookEnv.isQQ()) {
-                        if (HookEnv.requireMinQQVersion(QQVersion.QQ_9_3_0_BETA_36720)) {
-                            NSign.c().a(toServiceMsg, cmd).sign.toHexString()
-                        } else {
-                            QSign.c().a(toServiceMsg, cmd).sign.toHexString()
-                        }
-                    } else {
-                        TSign.e().a(toServiceMsg, cmd).sign.toHexString()
-                    }
+
+                    val (instance, method) = signer
+                    val sign = instance
+                        .invokeAs<QQSecuritySign.SignResult>(method, toServiceMsg, cmd)
+                        .sign
+                        .toHexString()
 
                     Intent(ACTION_SIGN_RESULT).apply {
                         putExtra("sign", sign)
@@ -171,37 +160,64 @@ class GetSign : IAction, DexKitTask, InputRootInitCallback {
         ReceiverRegistry.register(app, requestReceiver, filter)
     }
 
-    override fun getQueryMap(): Map<String, BaseMatcher> = buildMap {
-        if (HookEnv.isQQ()) {
-            this["InputRootInit"] = FindMethod().apply {
-                searchPackages("com.tencent.mobileqq.aio.input")
+    override fun getCacheKeys(): Set<String> {
+        return setOf(TASK_INPUT_ROOT_INIT, TASK_GET_SIGN)
+    }
+
+    override fun execute(bridge: DexKitBridge, cache: MutableMap<String, String>) {
+        if (!HookEnv.isQQ()) return
+
+        with(bridge) {
+            findAndCache(cache, TASK_GET_SIGN) {
+                searchPackages("com.tencent.mobileqq.msf.core")
                 matcher {
-                    usingEqStrings("binding", "inputRoot",
+                    usingEqStrings("invoke getSign start", "invoke getSign end")
+                }
+            }
+
+            val found = findMethod(FindMethod().apply {
+                searchPackages("com.tencent.mobileqq.aio.input.simpleui")
+                matcher {
+                    usingEqStrings(
+                        "binding", "inputRoot",
                         "findViewById(...)", "getContext(...)", "sendBtn"
                     )
                 }
-            }
+            }).singleOrNull()?.descriptor
+
+            cache[TASK_INPUT_ROOT_INIT] = found ?: findMethod(FindMethod().apply {
+                searchPackages("com.tencent.mobileqq.aio.input.simpleui")
+                matcher {
+                    usingEqStrings("inputRoot.findViewById(R.id.send_btn)")
+                }
+            }).singleOrNull()?.descriptor ?: ""
         }
     }
 
-    private fun getActiveEditText(): EditText? {
-        val activity = runCatching { QQInterfaces.topActivity }.getOrNull() ?: return null
-        val decorView = activity.window?.decorView ?: return null
-        return findAioEditText(decorView)
+    private fun DexKitBridge.findAndCache(
+        cache: MutableMap<String, String>,
+        key: String,
+        init: FindMethod.() -> Unit
+    ) {
+        findMethod(FindMethod().apply(init))
+            .singleOrNull()?.descriptor
+            .let { cache[key] = it ?: "" }
     }
 
-    private fun findAioEditText(view: View): EditText? {
-        if (view is EditText && view.isShown) {
-            return view
-        }
-        if (view is ViewGroup) {
-            for (i in 0 until view.childCount) {
-                val child = view.getChildAt(i)
-                val res = findAioEditText(child)
-                if (res != null) return res
+    @SuppressLint("DiscouragedApi")
+    private fun getAIOEditText(): EditText? {
+        return runCatching {
+            QQInterfaces.topActivity.let { activity ->
+                val resId = activity.resources.getIdentifier(
+                    "input",
+                    "id",
+                    activity.packageName
+                )
+                if (resId != 0) {
+                    activity.findViewById<EditText>(resId)
+                } else null
             }
-        }
-        return null
+        }.getOrNull()
     }
 
     private fun createToServiceMsg(cmd: String = "MessageSvc.PbSendMsg", uin: String): ToServiceMsg {
@@ -211,9 +227,11 @@ class GetSign : IAction, DexKitTask, InputRootInitCallback {
     companion object {
         private const val ACTION_REQUEST_SIGN = "com.owo233.tcqt.GET_SIGN_REQUEST"
         private const val ACTION_SIGN_RESULT = "com.owo233.tcqt.GET_SIGN_RESULT"
+        private const val TASK_INPUT_ROOT_INIT = "InputRootInit"
+        private const val TASK_GET_SIGN = "getSign"
     }
 }
 
 fun interface InputRootInitCallback {
-    fun onBtnLongClick(text: String, sendBtn: Button, editText: EditText, inputRoot: ViewGroup)
+    fun onBtnLongClick(sendBtn: Button, editText: EditText)
 }
