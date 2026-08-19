6f4375e5d51ba22e19a9272ee32950eb68cedf8f
owo233 <ver@vip.qq.com>
2026-08-19 23:31:25 +0800

    fix: 移除回复消息不存在限制 需在 9.1.75版本及以上生效

--- app/src/main/java/com/owo233/tcqt/hooks/func/activity/RemoveReplyMsgCheck.kt
+++ app/src/main/java/com/owo233/tcqt/hooks/func/activity/RemoveReplyMsgCheck.kt
@@ -5,6 +5,7 @@ import com.owo233.tcqt.HookEnv
 import com.owo233.tcqt.annotations.RegisterAction
 import com.owo233.tcqt.ext.ActionProcess
 import com.owo233.tcqt.ext.IAction
+import com.owo233.tcqt.utils.QQVersion
 import com.owo233.tcqt.utils.hook.hookMethodAfter
 import com.tencent.qqnt.kernel.nativeinterface.QQNTWrapperUtil
 import com.tencent.qqnt.kernel.nativeinterface.ReplyMsgMainInfo
@@ -12,24 +13,24 @@ import com.tencent.qqnt.kernel.nativeinterface.ReplyMsgMainInfo
 @RegisterAction
 class RemoveReplyMsgCheck : IAction {
 
+    override val key: String get() = "remove_reply_msg_check"
     override val name: String get() = "移除回复消息不存在限制"
     override val desc: String get() = "常见于解决‘转发的聊天记录中不包含该内容’的提示，允许查看回复消息。"
     override val uiTab: String get() = "界面"
 
+    override fun onInit(): Boolean {
+        return HookEnv.isQQ() && HookEnv.requireMinQQVersion(QQVersion.QQ_9_1_75)
+    }
+
     override fun onRun(app: Application, process: ActionProcess) {
-        if (HookEnv.isQQ()) {
-            QQNTWrapperUtil.CppProxy::class.java.hookMethodAfter({
-                name = "findSourceOfReplyMsgFrom"
-                paramTypes = arrayOf(arrayList, ReplyMsgMainInfo::class.java)
-            }) { param ->
-                val result = param.result as Long
-                if (result == 0L) {
-                    param.result = 1L
-                }
+        QQNTWrapperUtil.CppProxy::class.java.hookMethodAfter({
+            name = "findSourceOfReplyMsgFrom"
+            paramTypes = arrayOf(arrayList, ReplyMsgMainInfo::class.java)
+        }) { param ->
+            val result = param.result as Long
+            if (result == 0L) {
+                param.result = 1L
             }
         }
     }
-
-    override val key: String get() = "remove_reply_msg_check"
-    override val processes: Set<ActionProcess> get() = setOf(ActionProcess.MAIN)
 }
